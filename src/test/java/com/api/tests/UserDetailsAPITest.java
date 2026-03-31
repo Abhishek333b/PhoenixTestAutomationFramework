42674b5a87108d8e2ecef64c0eb32074582ab169
Abhishek <abhi.bhagat333@gmail.com>
2026-03-31 22:30:10 +0530

    Created AuthToken provider Manage the token for differnt roles Role:hold all the user roles of application UerdetailsApiest: CountAPItest: MasterAPI:

--- src/test/java/com/api/tests/UserDetailsAPITest.java
+++ src/test/java/com/api/tests/UserDetailsAPITest.java
@@ -1,5 +1,6 @@
 package com.api.tests;
 
+import static com.api.utils.ConfigManager.getProperty;
 import static io.restassured.RestAssured.given;
 
 import java.io.IOException;
@@ -7,7 +8,9 @@ import java.io.IOException;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
-import static com.api.utils.ConfigManager.*;
+import static com.api.constant.Role.*;
+
+import static com.api.utils.AuthTokenProvider.*;
 
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
@@ -19,7 +22,7 @@ public class UserDetailsAPITest {
 	public void userDetailsAPITest() throws IOException {
 		
 		
-		Header authHeader = new Header("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RfbmFtZSI6ImZkIiwibGFzdF9uYW1lIjoiZmQiLCJsb2dpbl9pZCI6ImlhbWZkIiwibW9iaWxlX251bWJlciI6Ijg4OTk3NzY2NTUiLCJlbWFpbF9pZCI6Im1hcmtAZ21haWwuY29tIiwicGFzc3dvcmQiOiI1ZjRkY2MzYjVhYTc2NWQ2MWQ4MzI3ZGViODgyY2Y5OSIsInJlc2V0X3Bhc3N3b3JkX2RhdGUiOm51bGwsImxvY2tfc3RhdHVzIjowLCJpc19hY3RpdmUiOjEsIm1zdF9yb2xlX2lkIjo1LCJtc3Rfc2VydmljZV9sb2NhdGlvbl9pZCI6MSwiY3JlYXRlZF9hdCI6IjIwMjEtMTEtMDNUMDg6MDY6MjMuMDAwWiIsIm1vZGlmaWVkX2F0IjoiMjAyMS0xMS0wM1QwODowNjoyMy4wMDBaIiwicm9sZV9uYW1lIjoiRnJvbnREZXNrIiwic2VydmljZV9sb2NhdGlvbiI6IlNlcnZpY2UgQ2VudGVyIEEiLCJpYXQiOjE3NzQ4NDUzODZ9.dGuVZvw_IGABXF61PIELnY6m0vV_TDTsmf04IU9knTo");
+		Header authHeader = new Header("Authorization",getToken(SUP));
 		given()
 		.baseUri(getProperty("BASE_URI"))
 		.and()
