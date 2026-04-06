9a6c35b9d5495c9890ad375cdfa73a722bcf1919
Abhishek <abhi.bhagat333@gmail.com>
2026-04-06 18:56:36 +0530

    Introduced Enums for Handing codes for the create Job API Test Reduce Magic number issue in the test script

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -12,7 +12,14 @@ import java.util.List;
 import java.time.Instant;
 import org.testng.annotations.Test;
 
+import com.api.constant.Model;
+import com.api.constant.OEM;
+import com.api.constant.Platform;
+import com.api.constant.Problem;
+import com.api.constant.Product;
 import com.api.constant.Role;
+import com.api.constant.Service_Location;
+import com.api.constant.Warrenty_Status;
 import com.api.pojo.CreateJobPayload;
 import com.api.pojo.Customer;
 import com.api.pojo.CustomerAddress;
@@ -26,19 +33,19 @@ public class CreateJobAPITest {
 	
 	@Test
 	public void createJobAPITest() {
-		System.out.println(Instant.now().minus(10,ChronoUnit.DAYS));
-		System.out.println("######################");
+		
 		Customer customer = new Customer("Abhi", "Bhagat", "7676767786", "", "abhi@gmail.com", "");
 		
 		CustomerAddress customerAddress = new CustomerAddress("A 12", "Abg c", "stghj t", "erhj t", "rtghj ee", "411056", "india", "maharashtra");
-		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "59904753563802", "59904753563802", "59904753563802", getTimeWithDaysAgo(10), 1, 1);
-		Problems problems = new Problems(1, "battery issue");
+		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "29904753563802", "29904753563802", "29904753563802", getTimeWithDaysAgo(10), 
+			Product.NEXUS_2.getCode(),Model.NEXUS_2_BLUE.getCode());
+		Problems problems = new Problems(Problem.SMARTPHONE_IS_RUNNING_SLOW.getCode(), "battery issue");
 		
 		List<Problems> problemList = new ArrayList<>();
 		problemList.add(problems);
 		
 		
-		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 1,customer, customerAddress, customerProduct, problemList);
+		CreateJobPayload createJobPayload = new CreateJobPayload(Service_Location.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), Warrenty_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(),customer, customerAddress, customerProduct, problemList);
 		given()
 		
 		
