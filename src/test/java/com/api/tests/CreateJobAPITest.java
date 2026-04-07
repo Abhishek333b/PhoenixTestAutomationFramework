ec28fe69ccf9e442a1492dd1d29b8af8f9298639
Abhishek <abhi.bhagat333@gmail.com>
2026-04-07 09:16:31 +0530

    added descriptions and groups to test introduced setup method for payload  creation outside test

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -1,15 +1,17 @@
 package com.api.tests;
 
+import static com.api.utils.DateTimeUtil.getTimeWithDaysAgo;
+import static com.api.utils.SpecUtil.requestSpecWithAuth;
+import static com.api.utils.SpecUtil.responseSpec_OK;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.startsWith;
 
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
-import java.time.Instant;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.api.constant.Model;
@@ -25,16 +27,13 @@ import com.api.pojo.Customer;
 import com.api.pojo.CustomerAddress;
 import com.api.pojo.CustomerProduct;
 import com.api.pojo.Problems;
-import static com.api.utils.DateTimeUtil.*;
-import com.api.utils.SpecUtil;
 
 public class CreateJobAPITest {
+ private	CreateJobPayload createJobPayload;
 	
-	
-	@Test
-	public void createJobAPITest() {
-		
-		Customer customer = new Customer("Abhi", "Bhagat", "7676767786", "", "abhi@gmail.com", "");
+ @BeforeMethod(description = "creating createjob api request payload")
+ public void setup() {
+         Customer customer = new Customer("Abhi", "Bhagat", "7676767786", "", "abhi@gmail.com", "");
 		
 		CustomerAddress customerAddress = new CustomerAddress("A 12", "Abg c", "stghj t", "erhj t", "rtghj ee", "411056", "india", "maharashtra");
 		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "29904753563802", "29904753563802", "29904753563802", getTimeWithDaysAgo(10), 
@@ -43,17 +42,22 @@ public class CreateJobAPITest {
 		
 		List<Problems> problemList = new ArrayList<>();
 		problemList.add(problems);
+		 createJobPayload = new CreateJobPayload(Service_Location.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), Warrenty_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(),customer, customerAddress, customerProduct, problemList);
 		
 		
-		CreateJobPayload createJobPayload = new CreateJobPayload(Service_Location.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), Warrenty_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(),customer, customerAddress, customerProduct, problemList);
+	}
+	
+	@Test(description = "Verifying if create api is giving correct response ",groups={"api","regression","smoke"})
+	public void createJobAPITest() {
+		
+		
+		
 		given()
-		
-		
-		.spec(SpecUtil.requestSpecWithAuth(Role.FD,createJobPayload))
+		.spec(requestSpecWithAuth(Role.FD,createJobPayload))
 		.when()
 		.post("/job/create")
 		.then()
-		.spec(SpecUtil.responseSpec_OK())
+		.spec(responseSpec_OK())
 		.body(matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
 		.body("message",equalTo("Job created successfully. "))
 		.body("data.mst_service_location_id",equalTo(1))
