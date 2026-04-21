290970d8edaca94c778b7399727ce77accf50894
Abhishek <abhi.bhagat333@gmail.com>
2026-04-21 15:18:22 +0530

    Created CustomerDao and CustomerDBModel for Req 13

---
+++ src/test/java/com/database/dao/DemoDaoRunner.java
@@ -0,0 +1,19 @@
+package com.database.dao;
+
+import java.sql.SQLException;
+
+import com.api.pojo.Customer;
+import com.database.model.CustomerDBModel;
+
+public class DemoDaoRunner {
+
+	public static void main(String[] args) throws SQLException {
+		CustomerDBModel customerDBData	=CustomerDao.getCustomerInfo();
+		System.out.println(customerDBData);
+		System.out.println(customerDBData.getFirst_name());
+		System.out.println(customerDBData.getEmail_id());
+		System.out.println(customerDBData.getMobile_number());
+		Customer customer = new Customer("Ab", "b", "3434343343", "", "abhi@gmail.com", "");
+	}
+
+}
---
+++ src/test/java/com/database/model/DemoRunner.java
@@ -0,0 +1,13 @@
+package com.database.model;
+
+public class DemoRunner {
+	public static void main(String[] args) {
+
+		CustomerDBModel customer =  new CustomerDBModel("abhi", "bhagat", "8989899989", "", "abhi@gmail.com", "");
+	
+		System.out.println(customer);
+		
+		
+		
+	}
+}
