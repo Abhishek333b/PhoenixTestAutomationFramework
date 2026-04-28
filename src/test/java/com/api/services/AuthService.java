0364bd5388acdc6c650baa7f8fe0509465d1a535
Abhishek <abhi.bhagat333@gmail.com>
2026-04-28 10:09:07 +0530

    Created Auth Service class to maintain all the login api at one place

---
+++ src/test/java/com/api/services/AuthService.java
@@ -0,0 +1,22 @@
+package com.api.services;
+
+import static com.api.utils.SpecUtil.requestSpec;
+import com.api.pojo.request.model.UserCredentials;
+import static io.restassured.RestAssured.given;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+
+public class AuthService {
+
+	private static final String LOGIN_ENDPOINT="login";
+	public Response ligin(UserCredentials userCredentials) {
+		
+		Response response=		given()
+		.spec(requestSpec(userCredentials))
+		.when()
+		.post(LOGIN_ENDPOINT);
+		
+		return response;
+	}
+}
---
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -0,0 +1,40 @@
+package com.api.tests;
+
+import static com.api.utils.SpecUtil.responseSpec_OK;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.api.pojo.request.model.UserCredentials;
+import com.api.services.AuthService;
+
+import io.restassured.response.Response;
+
+public class LoginAPITest {
+
+
+	private UserCredentials userCredentials;
+	private AuthService authService;
+	
+	@BeforeMethod(description = "Create the Payload for the Login API")
+	public void setup() {
+		userCredentials	 = new UserCredentials("iamfd", "password");
+		authService = new AuthService();
+	}
+	
+	@Test(description = "Verifying if login api is working for FD user", groups ={"api", "regression","smoke"}  )
+	public void loginAPITest() throws IOException {
+
+		authService.ligin(userCredentials)
+		.then().spec(responseSpec_OK())
+		.body("message", equalTo("Success"))
+		.and()
+		.body(matchesJsonSchemaInClasspath("response-schema/LoginResponseSchema.json"));
+
+	}
+
+}
